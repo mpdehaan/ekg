20e5c1b4eeb3e8445844887bd41b46e6325af8f0
Michael DeHaan <mdehaan@redhat.com>
2008-08-21 16:28:07 -0400

    Added db.rb (missing earlier), compute post ratios

---
+++ v2/db.rb
@@ -0,0 +1,41 @@
+require 'active_record'
+
+class Db 
+
+   def connect()
+       ActiveRecord::Base.establish_connection(
+           :adapter => "sqlite3",
+           :dbfile  => "sqlite3.db"
+       )
+   end
+
+   def setup()
+       begin
+           ActiveRecord::Schema.define do
+               create_table :posts do |table|
+                   table.column :url, :string
+                   table.column :subject, :string
+                   table.column :list_id, :string
+                   table.column :from_domain, :string
+                   table.column :from_addr, :string
+                   table.column :sent_date, :date
+               end
+               create_table :scans do |table|
+                   table.column :url, :string
+               end
+           end
+       rescue
+           puts "using existing database"
+       end
+   end
+
+end
+
+class Post < ActiveRecord::Base
+  belongs_to :posts
+end
+
+class Scan < ActiveRecord::Base
+  belongs_to :scans
+end 
+
--- v2/grapher.rb
+++ v2/grapher.rb
@@ -19,10 +19,12 @@ class Grapher
    def run()
       lists = Post.connection.select_values("select DISTINCT(list_id) from posts")
       lists.each do |list|
+            posts = Post.connection.select_values("select COUNT(*) from posts where list_id = '#{list}'")[0].to_f()
             domains = Post.connection.select_values("select DISTINCT(from_domain) from posts where list_id='#{list}'")
             domains.each do |domain|
-               count = Post.connection.select_values("select count(*) from posts where list_id='#{list}' and from_domain='#{domain}'")
-               puts "#{list} #{domain} #{count}"
+               count = Post.connection.select_values("select count(*) from posts where list_id='#{list}' and from_domain='#{domain}'")[0].to_f()
+               ratio = count / posts 
+               puts "#{list} #{domain} #{count} #{ratio}"
             end
       end
    
