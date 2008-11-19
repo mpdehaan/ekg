1dce6bc84ec071ab9323a576cf233eea7a462493
Michael DeHaan <mdehaan@redhat.com>
2008-11-19 18:41:10 -0500

    Four patches from Steve Milner.

--- v2/db.rb
+++ v2/db.rb
@@ -1,3 +1,12 @@
+# Copyright 2008, Red Hat, Inc
+#
+# This software may be freely redistributed under the terms of the GNU
+# general public license.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 require 'active_record'
 
 class Db 
@@ -38,4 +47,3 @@ end
 class Scan < ActiveRecord::Base
   belongs_to :scans
 end 
-
--- v2/ekg.rb
+++ v2/ekg.rb
@@ -1,4 +1,13 @@
 #!/usr/bin/ruby
+#
+# Copyright 2008, Red Hat, Inc
+#
+# This software may be freely redistributed under the terms of the GNU
+# general public license.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 require 'getoptlong'
 require 'active_record'
@@ -8,8 +17,13 @@ require 'scanner'
 require 'finder'
 require 'db'
 
-#config = "/etc/ekg/settings"
-config = "settings" # until packaged
+if File.exists?("/etc/ekg/settings"):
+   # Use system default if possible
+   config = "/etc/ekg/settings"
+else
+   # assume a local run
+   config = "settings"
+end
 
 is_scanning = false
 is_graphing = false
@@ -22,18 +36,23 @@ opts = GetoptLong.new(
    [ "--find",   "-f", GetoptLong::NO_ARGUMENT ]
 )
 
-opts.each { |opt, arg|
-   case opt
-   when "--config"
-      config_file = arg
-   when "--scan"
-      is_scanning = true
-   when "--graph"
-      is_graphing = true
-   when "--find"
-      is_finding =  true
-   end
-}
+begin
+   opts.each { |opt, arg|
+      case opt
+      when "--config"
+         config_file = arg
+      when "--scan"
+         is_scanning = true
+      when "--graph"
+         is_graphing = true
+      when "--find"
+         is_finding =  true
+      end
+   }
+rescue GetoptLong::InvalidOption => ex
+   # Give a different exit code if we get an invalid option
+   exit(2)
+end
 
 if not (is_scanning or is_graphing or is_finding)
    puts "nothing to do"
@@ -47,4 +66,3 @@ db.setup()
 Scanner.new(config).run if is_scanning
 Grapher.new(config).run if is_graphing
 Finder.new(config).run  if is_finding
-
